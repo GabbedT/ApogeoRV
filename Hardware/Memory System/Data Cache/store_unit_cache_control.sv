29e8d2ad597bcc946f286258b9bdfbadaf8626be
GabbedT <gabrieletripi02@gmail.com>
2022-08-28 20:06:20 +0200

    Added description

--- Hardware/Memory System/Data Cache/store_unit_cache_control.sv
+++ Hardware/Memory System/Data Cache/store_unit_cache_control.sv
@@ -1,3 +1,39 @@
+// MIT License
+//
+// Copyright (c) 2021 Gabriele Tripi
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
+// FILE NAME : store_unit_cache_control.sv
+// DEPARTMENT : 
+// AUTHOR : Gabriele Tripi
+// AUTHOR'S EMAIL : tripi.gabriele2002@gmail.com
+// -------------------------------------------------------------------------------------
+// RELEASE HISTORY
+// VERSION : 1.0 
+// DESCRIPTION : Cache controller for store operations. It provide a simple interface 
+//               to cache for the store unit and memory unit. It manages the cache 
+//               and memory transactions, it is mainly accessed by the store unit,
+//               memory unit can make an invalidation request to ensure memory coherency
+// -------------------------------------------------------------------------------------
+
 `ifndef STORE_UNIT_CACHE_CONTROL_SV
     `define STORE_UNIT_CACHE_CONTROL_SV
 
