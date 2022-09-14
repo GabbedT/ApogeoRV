ee8d8b1fcade13610201cd20772ae0d2a6a2ec9a
GabbedT <gabrieletripi02@gmail.com>
2022-09-14 15:58:29 +0200

    Added files for future

---
+++ Hardware/Include/control_status_registers_pkg.sv
@@ -0,0 +1,12 @@
+`ifndef CONTROL_STATUS_REGISTERS_SV
+    `define CONTROL_STATUS_REGISTERS_SV
+
+package control_status_registers_pkg;
+
+
+
+endpackage : control_status_registers_pkg
+
+import control_status_registers_pkg::*;
+
+`endif 
