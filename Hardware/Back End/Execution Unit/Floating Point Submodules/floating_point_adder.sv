507f174a2d5cfea7439d56741b0e6985d0108193
GabbedT <gabrieletripi02@gmail.com>
2022-11-07 15:55:50 +0100

    Fixed floating point multiplier and added clock enable input

--- Hardware/Back End/Execution Unit/Floating Point Submodules/floating_point_adder.sv
+++ Hardware/Back End/Execution Unit/Floating Point Submodules/floating_point_adder.sv
@@ -6,6 +6,11 @@
 
 module floating_point_adder (
     input  logic             clk_i,
+
+    `ifdef FPGA 
+        input logic clk_en_i, 
+    `endif 
+
     input  logic             rst_n_i,
     input  float32_t         addend_A_i,
     input  float32_t         addend_B_i,
@@ -29,7 +34,7 @@ module floating_point_adder (
         always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : shift_register
             if (!rst_n_i) begin 
                 valid_bit_pipe <= '0;
-            end else begin 
+            end else `ifdef FPGA if (clk_en_i) `endif begin 
                 valid_bit_pipe <= {valid_bit_pipe[2:0], data_valid_i}; 
             end
         end : shift_register
@@ -117,12 +122,17 @@ module floating_point_adder (
     logic [22:0] minor_addend_mantissa_stg0;
 
         always_ff @(posedge clk_i) begin : stage0_register
-            major_addend_stg0 <= major_addend;
-            minor_addend_sign_stg0 <= minor_addend.sign;
-            minor_addend_mantissa_stg0 <= minor_addend.mantissa;
-            minor_hidden_bit_stg0 <= minor_hidden_bit;
-            exp_subtraction_stg0 <= exp_subtraction_abs[7:0];
-            invalid_operation_stg0 <= invalid_operation;
+            `ifdef FPGA if (clk_en_i) begin `endif
+                major_addend_stg0 <= major_addend;
+
+                minor_addend_sign_stg0 <= minor_addend.sign;
+                minor_addend_mantissa_stg0 <= minor_addend.mantissa;
+                minor_hidden_bit_stg0 <= minor_hidden_bit;
+
+                exp_subtraction_stg0 <= exp_subtraction_abs[7:0];
+
+                invalid_operation_stg0 <= invalid_operation;
+            `ifdef FPGA end `endif 
         end : stage0_register
     
 
@@ -143,52 +153,22 @@ module floating_point_adder (
     assign mantissa_aligned = mantissa_shifted[47:24];
 
 
-    /* Compute rounding bits (Guard, Round, Sticky) */
-    round_bits_t round_bits;
-
-    assign round_bits.guard = mantissa_shifted[23];
-    assign round_bits.round = mantissa_shifted[22];
-    
-    /* Compute the sticky bit by counting the number of trailing zeroes
-     * in the shifted out bits of mantissa (don't consider guard and round) */
-    logic [4:0] trailing_zeros;
-
-    /* Counting the trailing zeros of a number is counting the leading zeroes of that
-     * number with reversed bits */
-    logic [23:0] reversed_shifted_mantissa;
-
-        always_comb begin : reverse_bits
-
-            reversed_shifted_mantissa[1:0] = '1;
-
-            for (int i = 2; i < 24; ++i) begin 
-                reversed_shifted_mantissa[i] = mantissa_shifted[23 - i];
-            end
-        end : reverse_bits
-
-    count_leading_zeros #(24) ctz_mantissa (
-        .operand_i     ( reversed_shifted_mantissa  ),
-        .lz_count_o    ( trailing_zeros             ),
-        .is_all_zero_o (    /* NOT CONNECTED */     )
-    );
-
-    /* Sticky bit is set if there are less than 22 trailing zeroes */
-    assign round_bits.sticky = (trailing_zeros != 5'd22);
-
-
     /* Stage register nets */
     logic        minor_addend_sign_stg1;
     logic        invalid_operation_stg1;
-    logic [23:0] minor_addend_mantissa_stg1;
+    logic [23:0] minor_addend_mantissa_stg1, minor_shifted_mantissa_stg1;
     float32_t    major_addend_stg1;
-    round_bits_t round_bits_stg1;
 
         always_ff @(posedge clk_i) begin : stage1_register
-            minor_addend_sign_stg1 <= minor_addend_sign_stg0;
-            minor_addend_mantissa_stg1 <= mantissa_aligned;
-            invalid_operation_stg1 <= invalid_operation_stg0;
-            major_addend_stg1 <= major_addend_stg0;
-            round_bits_stg1 <= round_bits;
+            `ifdef FPGA if (clk_en_i) begin `endif
+                minor_addend_sign_stg1 <= minor_addend_sign_stg0;
+                minor_addend_mantissa_stg1 <= mantissa_aligned;
+                minor_shifted_mantissa_stg1 <= mantissa_shifted[23:0];
+
+                invalid_operation_stg1 <= invalid_operation_stg0;
+
+                major_addend_stg1 <= major_addend_stg0;
+            `ifdef FPGA end `endif 
         end : stage1_register
 
 
@@ -207,8 +187,8 @@ module floating_point_adder (
         always_comb begin : sum_logic
             case ({major_addend_stg1.sign, minor_addend_sign_stg1})
                 2'b00: begin
-                    major_mantissa = {1'b0, major_hidden_bit, major_addend_stg1.mantissa};
-                    minor_mantissa = {1'b0, minor_addend_mantissa_stg1};
+                    major_mantissa =  {1'b0, major_hidden_bit, major_addend_stg1.mantissa};
+                    minor_mantissa =  {1'b0, minor_addend_mantissa_stg1};
                 end
 
                 2'b01: begin
@@ -229,13 +209,13 @@ module floating_point_adder (
         end : sum_logic
 
     /* Result mantissa, consider also carry and hidden bits */
-    logic [24:0] result_mantissa;
+    logic [25:0] result_mantissa;
     logic [23:0] result_mantissa_abs;
-    logic        carry_result;
 
-    assign {carry_result, result_mantissa} = major_mantissa + minor_mantissa;
+    assign result_mantissa = major_mantissa + minor_mantissa;
 
-    assign result_mantissa_abs = result_mantissa[24] ? -result_mantissa[23:0] : result_mantissa[23:0];
+    /* Compute the absolute value of the mantissa if the last bit of the result is 1 and the major number is negative */
+    assign result_mantissa_abs = major_mantissa[24] ? -result_mantissa[23:0] : result_mantissa[23:0];
 
 
     /* Check signs for normalization */
@@ -246,20 +226,24 @@ module floating_point_adder (
 
     /* Stage register nets */
     float32_t    result_stg2;
-    round_bits_t round_bits_stg2;
+    logic [23:0] minor_shifted_mantissa_stg2;
     logic        hidden_bit_result_stg2;
     logic        carry_result_stg2;
     logic        signs_different_stg2;
     logic        invalid_operation_stg2;
 
         always_ff @(posedge clk_i) begin : stage2_register
-            /* The last bit of the mantissa addition rapresent the sign of the result */
-            result_stg2 <= {result_mantissa[24], major_addend_stg1.exponent, result_mantissa_abs[22:0]};
-            round_bits_stg2 <= round_bits_stg1;
-            carry_result_stg2 <= carry_result;
-            signs_different_stg2 <= signs_different;
-            invalid_operation_stg2 <= invalid_operation_stg1;
-            hidden_bit_result_stg2 <= result_mantissa_abs[23];
+            `ifdef FPGA if (clk_en_i) begin `endif
+                /* The last bit of the mantissa addition rapresent the sign of the result */
+                result_stg2 <= {major_addend_stg1.sign, major_addend_stg1.exponent, result_mantissa_abs[22:0]};
+
+                /* Carry is valid only if there was an addition */
+                carry_result_stg2 <= result_mantissa[25] & !signs_different;
+                signs_different_stg2 <= signs_different;
+                invalid_operation_stg2 <= invalid_operation_stg1;
+                hidden_bit_result_stg2 <= result_mantissa_abs[23];
+                minor_shifted_mantissa_stg2 <= minor_shifted_mantissa_stg1;
+            `ifdef FPGA end `endif 
         end : stage2_register
 
 
@@ -272,8 +256,8 @@ module floating_point_adder (
 
     count_leading_zeros #(24) clz_mantissa (
         .operand_i     ( {hidden_bit_result_stg2, result_stg2.mantissa} ),
-        .lz_count_o    ( leading_zeros                ),
-        .is_all_zero_o (    /* NOT CONNECTED */       )
+        .lz_count_o    ( leading_zeros                                  ),
+        .is_all_zero_o (    /* NOT CONNECTED */                         )
     );
 
 
@@ -286,19 +270,26 @@ module floating_point_adder (
     /* Result of subtraction between the exponent and leading zeros number. 
      * The result is 8 bits because it accounts for the sign bit since the 
      * exponent operand is biased */
-    logic [8:0] exponent_normalized;
+    logic [8:0] exponent_sub_normalized;
 
-    assign exponent_normalized = {1'b0, result_stg2.exponent} - leading_zeros;
+    assign exponent_sub_normalized = {1'b0, result_stg2.exponent} - leading_zeros;
 
 
     /* Shifted out bit after normalization */
     logic shifted_out_bit;
+
+    /* Don't leave the shifted out bits during the normalization after a subtraction */
+    logic [47:0] full_result_shifted_mantissa;
+
+    /* Shifted mantissa after normalization of an addition */
+    logic [23:0] result_shifted_one;
     
         always_comb begin : normalization_logic
             /* Default values */
             final_result.sign = result_stg2.sign;
             final_result.exponent = result_stg2.exponent;
             final_result.mantissa = result_stg2.mantissa;
+            full_result_shifted_mantissa = '0;
             final_overflow = 1'b0; 
             final_underflow = 1'b0;
 
@@ -307,8 +298,9 @@ module floating_point_adder (
                 /* If there was a carry and the signs are equals, so mantissas
                  * were added, then normalize the result by shifting the mantissa
                  * by one and incrementing the exponent */
-                2'b01: begin
-                    {final_result.mantissa, shifted_out_bit} = {hidden_bit_result_stg2, result_stg2.mantissa} >> 1;
+                2'b10: begin
+                    result_shifted_one = {hidden_bit_result_stg2, result_stg2.mantissa} >> 1;
+                    final_result.mantissa = result_shifted_one[22:0];
 
                     if (final_result.exponent == MAX_EXP) begin
                         final_result.exponent = result_stg2.exponent;
@@ -321,18 +313,22 @@ module floating_point_adder (
                     end
                 end
 
-                /* If there are N leading zeros and the sign are, o mantissas 
+                /* If there are N leading zeros and the sign are, so mantissas 
                  * were subtracted, then shift the mantissa left by N and 
                  * subtract N from the exponent */
-                2'b10: begin
-                    if (exponent_normalized[8] | (exponent_normalized == '0)) begin
+                2'b01: begin
+                    if (exponent_sub_normalized[8] | (exponent_sub_normalized == '0)) begin
                         final_result.exponent = MIN_EXP;
-                        {shifted_out_bit, final_result.mantissa} = {hidden_bit_result_stg2, result_stg2.mantissa} << (final_result.exponent - MIN_EXP);
+                        full_result_shifted_mantissa = {hidden_bit_result_stg2, result_stg2.mantissa, minor_shifted_mantissa_stg2} << (final_result.exponent - MIN_EXP);
+
+                        final_result.mantissa = full_result_shifted_mantissa[46:24];
 
                         final_underflow = 1'b1;
                     end else begin
                         final_result.exponent = result_stg2.exponent - leading_zeros;
-                        {shifted_out_bit, final_result.mantissa} = {hidden_bit_result_stg2, result_stg2.mantissa} << leading_zeros;
+                        full_result_shifted_mantissa = {hidden_bit_result_stg2, result_stg2.mantissa, minor_shifted_mantissa_stg2} << leading_zeros;
+
+                        final_result.mantissa = full_result_shifted_mantissa[45:23];
 
                         final_underflow = 1'b0;
                     end
@@ -351,6 +347,67 @@ module floating_point_adder (
         end : normalization_logic
 
 
+    /* Compute rounding bits (Guard, Round, Sticky) */
+    round_bits_t round_bits;
+
+    /* Counting the trailing zeros of a number is counting the leading zeroes of that
+     * number with reversed bits */
+    logic [21:0] reversed_shifted_mantissa;
+
+        always_comb begin
+            /* Default values */
+            round_bits.guard = 1'b0;
+            round_bits.round = 1'b0;
+            reversed_shifted_mantissa = '0;
+
+            case ({(carry_result_stg2 & !signs_different_stg2), ((leading_zeros != 5'b0) & signs_different_stg2)})
+                2'b01: begin
+                    round_bits.guard = result_stg2.mantissa[0];
+                    round_bits.round = minor_shifted_mantissa_stg2[23];
+
+                    /* Reverse bits to count trailing zeros */
+                    for (int i = 0; i < 22; ++i) begin
+                        /* Last bit is shifted out */
+                        reversed_shifted_mantissa[i] = minor_shifted_mantissa_stg2[22 - i];
+                    end
+                end
+
+                2'b10: begin
+                    round_bits.guard = full_result_shifted_mantissa[23];
+                    round_bits.round = full_result_shifted_mantissa[22];
+
+                    /* Reverse bits to count trailing zeros */
+                    for (int i = 0; i < 22; ++i) begin
+                        reversed_shifted_mantissa[i] = full_result_shifted_mantissa[21 - i];
+                    end
+                end
+
+                default: begin
+                    round_bits.guard = minor_shifted_mantissa_stg2[23];
+                    round_bits.round = minor_shifted_mantissa_stg2[22];  
+
+                    /* Reverse bits to count trailing zeros */
+                    for (int i = 0; i < 22; ++i) begin
+                        reversed_shifted_mantissa[i] = minor_shifted_mantissa_stg2[21 - i];
+                    end
+                end
+            endcase 
+        end
+
+    /* Compute the sticky bit by counting the number of trailing zeroes
+     * in the shifted out bits of mantissa (don't consider guard and round) */
+    logic [4:0] trailing_zeros;
+
+    count_leading_zeros #(24) ctz_mantissa (
+        .operand_i     ( {reversed_shifted_mantissa, 2'b11} ),
+        .lz_count_o    ( trailing_zeros                     ),
+        .is_all_zero_o (    /* NOT CONNECTED */             )
+    );
+
+    /* Sticky bit is set if there are less than 22 trailing zeroes */
+    assign round_bits.sticky = (trailing_zeros != 5'd22);
+
+
     /* Stage register nets */
     float32_t    result_stg3;
     round_bits_t round_bits_stg3;
@@ -358,11 +415,13 @@ module floating_point_adder (
     logic        invalid_operation_stg3;
         
         always_ff @(posedge clk_i) begin : stage3_register
-            result_stg3 <= final_result;
-            overflow_stg3 <= final_overflow;
-            underflow_stg3 <= final_underflow;
-            round_bits_stg3 <= round_bits_stg2;
-            invalid_operation_stg3 <= invalid_operation_stg2;
+            `ifdef FPGA if (clk_en_i) begin `endif
+                result_stg3 <= final_result;
+                overflow_stg3 <= final_overflow;
+                underflow_stg3 <= final_underflow;
+                round_bits_stg3 <= round_bits;
+                invalid_operation_stg3 <= invalid_operation_stg2;
+            `ifdef FPGA end `endif 
         end : stage3_register
 
 
