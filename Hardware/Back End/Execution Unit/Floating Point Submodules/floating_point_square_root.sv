9dbd2bf52ef415687a3559fe5811e25179b5b60b
GabbedT <gabrieletripi02@gmail.com>
2022-12-18 20:49:24 +0100

    Added description header, fixed bugs and added hardware for subnormal inputs numbers

--- Hardware/Back End/Execution Unit/Floating Point Submodules/floating_point_square_root.sv
+++ Hardware/Back End/Execution Unit/Floating Point Submodules/floating_point_square_root.sv
@@ -1,3 +1,38 @@
+// MIT License
+//
+// Copyright (c) 2021 Gabriele Tripi
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// ------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
+// FILE NAME : floating_point_square_root.sv
+// DEPARTMENT : 
+// AUTHOR : Gabriele Tripi
+// AUTHOR'S EMAIL : tripi.gabriele2002@gmail.com
+// ------------------------------------------------------------------------------------
+// RELEASE HISTORY
+// VERSION : 1.0 
+// DESCRIPTION : This module perform a floating point square root. It is a sequential 
+//               unit so it cannot accept new valid input until the result become 
+//               valid.
+// ------------------------------------------------------------------------------------
+
 `ifndef FLOATING_POINT_SQUARE_ROOT_SV
     `define FLOATING_POINT_SQUARE_ROOT_SV
 
@@ -41,8 +76,8 @@ module floating_point_square_root (
      * is the same as the input */
     logic operand_is_special;
 
-    assign operand_is_special = ((radicand_i.exponent == '0) & (radicand_i.mantissa == '0)) | 
-                                ((radicand_i.exponent == '1) & (radicand_i.mantissa == '0));
+    assign operand_is_special = ((radicand_i.exponent == '0) & (radicand_i.significand == '0)) | 
+                                ((radicand_i.exponent == '1) & (radicand_i.significand == '0));
 
     logic operand_is_special_CRT, operand_is_special_NXT;
 
@@ -63,16 +98,30 @@ module floating_point_square_root (
         end
 
 
+    /* Operand is a subnormal number */
+    logic radicand_is_subnormal_CRT, radicand_is_subnormal_NXT;
+
+        always_ff @(posedge clk_i) begin
+            if (clk_en_i) begin
+                radicand_is_subnormal_CRT <= radicand_is_subnormal_NXT;
+            end
+        end
+
+
     /* Hold the final result */
     float32_t result_CRT, result_NXT; 
 
     /* Hold the input for later processing */
     float32_t radicand_CRT, radicand_NXT;
+    logic     hidden_bit_CRT, hidden_bit_NXT;
+
+    assign hidden_bit = result_NXT.exponent != '0;
 
         always_ff @(posedge clk_i) begin
             if (clk_en_i) begin
                 result_CRT <= result_NXT;
                 radicand_CRT <= radicand_NXT;
+                hidden_bit_CRT <= hidden_bit_NXT;
             end
         end
         
@@ -91,38 +140,40 @@ module floating_point_square_root (
         end
         
 
+    logic sqrt_valid_in;
 
-    /* Hidden bit after mantissa */
-    logic hidden_bit;
-
-    assign hidden_bit = result_NXT.exponent != '0;
-
+        always_ff @(posedge clk_i) begin
+            if (clk_en_i) begin
+                sqrt_valid_in <= data_valid_i;
+            end
+        end
 
     /* Module nets */
-    logic [23:0] result_sqrt, remainder_sqrt;
-    logic        sqrt_valid;
+    logic [23:0] result_sqrt;
+    logic [24:0] remainder_sqrt;
+    logic        sqrt_valid_out;
 
     /* Integer square root module instantiation 
-     * for mantissa square root */
-    non_restoring_square_root #(48) mantissa_core_sqrt (
-        .clk_i        ( clk_i                                 ),    
-        .clk_en_i     ( clk_en_i                              ),
-        .data_valid_i ( data_valid_i                          ),
-        .rst_n_i      ( rst_n_i                               ),    
-        .radicand_i   ( {hidden_bit, result_NXT.mantissa, '0} ),
-        .root_o       ( result_sqrt                           ),     
-        .remainder_o  ( remainder_sqrt                        ),  
-        .data_valid_o ( sqrt_valid                            ),
-        .idle_o       (          /* NOT CONNECTED */          )
+     * for significand square root */
+    non_restoring_square_root #(48) significand_core_sqrt (
+        .clk_i        ( clk_i                                        ),    
+        .clk_en_i     ( clk_en_i                                     ),
+        .data_valid_i ( sqrt_valid_in                                ),
+        .rst_n_i      ( rst_n_i                                      ),    
+        .radicand_i   ( {hidden_bit_CRT, result_CRT.significand, '0} ),
+        .root_o       ( result_sqrt                                  ),     
+        .remainder_o  ( remainder_sqrt                               ),  
+        .data_valid_o ( sqrt_valid_out                               ),
+        .idle_o       (          /* NOT CONNECTED */                 )
     );
 
 
     logic [4:0] clz_number;
 
     count_leading_zeros #(24) clz (
-        .operand_i     ( {hidden_bit, radicand_i.mantissa} ),
-        .lz_count_o    ( clz_number                        ),
-        .is_all_zero_o (        /* NOT CONNECTED */        )
+        .operand_i     ( {hidden_bit, radicand_i.significand} ),
+        .lz_count_o    ( clz_number                           ),
+        .is_all_zero_o (        /* NOT CONNECTED */           )
     );
 
 
@@ -143,13 +194,19 @@ module floating_point_square_root (
         end
 
 
-    logic [23:0] normalized_mantissa;
+    /* Normalized radicand */
+    logic [23:0] normalized_significand;
     logic [8:0]  normalized_exponent;
+
+    /* Result exponent absolute value */
+    logic [4:0]  exponent_abs;  
 
         always_comb begin : fsm_logic
             /* Default values */
+            radicand_is_subnormal_NXT = radicand_is_subnormal_CRT;
             operand_is_special_NXT = operand_is_special_CRT;
             operand_is_nan_NXT = operand_is_nan_CRT;
+            hidden_bit_NXT = hidden_bit_CRT;
             radicand_NXT = radicand_CRT;
             result_NXT = result_CRT;
             sticky_NXT = sticky_CRT;
@@ -157,7 +214,7 @@ module floating_point_square_root (
             guard_NXT = guard_CRT;
             state_NXT = state_CRT;
             
-
+            data_valid_o = 1'b0;
             result_o = '0;
             invalid_operation_o = 1'b0;
 
@@ -165,23 +222,29 @@ module floating_point_square_root (
 
                 /*
                  *  The FSM is not completely idle, infact it 
-                 *  pre computes the exponent and mantissa for
+                 *  pre computes the exponent and significand for
                  *  the square root operation 
                  */
                 IDLE: begin
                     result_NXT.sign = radicand_i.sign;
 
-                    /* Normalize first the mantissa, increment the exponent accordingly */
-                    normalized_mantissa = {hidden_bit, radicand_i.mantissa} << clz_number;
-                    normalized_exponent = radicand_i.exponent - clz_number;
+                    /* Normalize first the significand, subtracting the exponent accordingly to the
+                     * number of shifts. We can optimize it by adding the leading zero number and 
+                     * setting a bit to recognize when the exponent is negative. That will save an
+                     * adder in the later normalization stage. This won't affect the normal number
+                     * because in that case the number of leading zeros will be 0 */
+                    normalized_significand = {hidden_bit, radicand_i.significand} << clz_number;
+                    normalized_exponent = radicand_i.exponent + clz_number;
+                    radicand_is_subnormal_NXT = normalized_exponent[8];
 
-                    /* Make the exponent even and divide it by two, shift the mantissa accordingly */
-                    result_NXT.exponent = (normalized_exponent - normalized_exponent[0]) >> 1;
-                    result_NXT.mantissa = normalized_mantissa << normalized_exponent[0];
+                    /* Make the exponent even shift the significand accordingly */
+                    result_NXT.exponent = normalized_exponent - normalized_exponent[0];
+                    result_NXT.significand = normalized_significand << normalized_exponent[0];
 
                     /* Check if the input is a NaN or a special number */
-                    operand_is_nan_NXT = (radicand_i.exponent == '0) & (radicand_i.mantissa != '0);
-                    operand_is_special_NXT <= operand_is_special;
+                    operand_is_nan_NXT = (radicand_i.exponent == '0) & (radicand_i.significand != '0);
+                    operand_is_special_NXT = operand_is_special;
+                    hidden_bit_NXT = radicand_i.exponent != '0;
 
                     /* Store the input */
                     radicand_NXT = radicand_i;
@@ -196,10 +259,10 @@ module floating_point_square_root (
                  *  a valid result 
                  */
                 SQRT: begin
-                    if (sqrt_valid) begin
+                    if (sqrt_valid_out) begin
                         state_NXT = VALID;
 
-                        result_NXT.mantissa = result_sqrt;
+                        result_NXT.significand = result_sqrt;
 
                         /* Rounding bits */
                         guard_NXT = remainder_sqrt[23];
@@ -225,7 +288,20 @@ module floating_point_square_root (
                         result_o = CANONICAL_NAN;
                         invalid_operation_o = 1'b1;
                     end else begin
-                        result_o = result_CRT;
+                        /* If the result exponent is negative normalize it 
+                         * back */
+                        if (radicand_is_subnormal_CRT) begin
+                            /* The effect is bringing back the exponent to 0 by shifting 
+                             * the significand by a number equals to the absolute value 
+                             * of the exponent */
+                            result_o.significand = result_CRT.significand >> result_CRT.exponent;
+                            result_o.exponent = '0;
+                        end else begin
+                            result_o.significand = result_CRT.significand;
+
+                            /* The exponent must be divided by 2 */
+                            result_o.exponent = result_CRT.exponent >> 1;
+                        end
                     end
 
                     /* Result is valid for 1 clock cycle then go IDLE */
@@ -239,7 +315,7 @@ module floating_point_square_root (
     assign inexact_o = guard_CRT | round_CRT | sticky_CRT;
 
     /* Square root result overflows only if the input is +infinity */
-    assign overflow_o = (radicand_CRT.sign) & (radicand_CRT.exponent == '1) & (radicand_CRT.mantissa == '0);
+    assign overflow_o = (radicand_CRT.sign) & (radicand_CRT.exponent == '1) & (radicand_CRT.significand == '0);
 
     assign round_bits_o.guard = guard_CRT;
     assign round_bits_o.round = round_CRT;
