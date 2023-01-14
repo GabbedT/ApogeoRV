babbcb46064a5dd3c5dca9b67ad65efe3264fd80
GabbedT <gabrieletripi02@gmail.com>
2023-01-14 01:56:20 +0100

    STU architecture updated

--- Hardware/Back End/Execution Unit/Memory Unit Submodules/store_unit.sv
+++ Hardware/Back End/Execution Unit/Memory Unit Submodules/store_unit.sv
@@ -1,7 +1,7 @@
 `ifndef STORE_UNIT_SV
     `define STORE_UNIT_SV
 
-`include "../../Include/Packages/rv32_instructions_pkg.sv"
+`include "../../Include/Packages/apogeo_pkg.sv"
 `include "../../Include/Headers/apogeo_configuration.svh"
 `include "../../Include/Headers/apogeo_memory_map.svh"
 `include "../../Include/Packages/data_memory_pkg.sv"
@@ -27,24 +27,6 @@ module store_unit (
      * STU can now transition in IDLE state */
     input logic data_accepted_i,
 
-
-    /* Store full, half word or a single byte */
-    output store_width_t store_width_o,
-
-    /* Data to store and store memory location
-     * output */
-    output data_word_t store_data_o,
-    output data_word_t store_address_o,
-
-    /* Functional unit status */
-    output logic idle_o,
-
-    /* Illegal memory access exception */
-    output logic illegal_access_o,
-
-    /* Data is valid */
-    output logic data_valid_o
-
     `ifdef CACHE_SYSTEM
         /* Cache controller has finished storing 
          * data into cache */
@@ -54,20 +36,46 @@ module store_unit (
         input  logic cache_ctrl_store_idle_i,
 
         /* Data properties */
-        output logic data_bufferable_o,
-        output logic data_cachable_o,
+        output logic cache_ctrl_bufferable_o,
+        output logic cache_ctrl_cachable_o,
 
         /* Cache controller store request */
         output logic cache_ctrl_store_o,
+
+        /* Data to store in cache */
+        output data_cache_data_t cache_ctrl_data_o,
+
+        /* Store location */
+        output data_cache_full_addr_t cache_ctrl_address_o,
+
+        /* Store width */
+        output store_width_t cache_ctrl_store_width_o,
     `else 
+        /* Enable pushing data into the buffer */
+        output logic data_bufferable_o,
+        
+        /* Store buffer is full */
+        input logic str_buf_full_i,
+
         /* Memory controller store request */
-        output logic memory_ctrl_store_o,
+        output logic str_buf_push_data_o,
 
-        /* Memory buffer is full */
-        output logic memory_ctrl_full_o,
+
+        /* Data to store, store memory location
+         * and store width output */
+        output store_buffer_entry_t str_buf_packet_o,
     `endif 
-);
 
+
+    /* Functional unit status */
+    output logic idle_o,
+ 
+    /* Illegal memory access exception */
+    output logic illegal_access_o,
+
+    /* Data is valid */
+    output logic data_valid_o
+);
 
 //------------//
 //  DATAPATH  //
@@ -87,20 +95,34 @@ module store_unit (
 
     /* Write cache and memory request signal */
     logic data_bufferable_CRT, data_bufferable_NXT; 
-    logic data_cachable_CRT, data_cachable_NXT; 
+
+    `ifdef CACHE_SYSTEM
+        logic data_cachable_CRT, data_cachable_NXT; 
+    `endif 
 
         always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin
             if (!rst_n_i) begin 
                 data_bufferable_CRT <= 1'b1;
-                data_cachable_CRT <= 1'b1;
+
+                `ifdef CACHE_SYSTEM
+                    data_cachable_CRT <= 1'b1;
+                `endif
             end else if (clk_en_i) begin 
                 data_bufferable_CRT <= data_bufferable_NXT;
-                data_cachable_CRT <= data_cachable_NXT;
+
+                `ifdef CACHE_SYSTEM
+                    data_cachable_CRT <= data_cachable_NXT;
+                `endif
             end
         end
 
-    assign data_bufferable_o = data_bufferable_CRT;
-    assign data_cachable_o = data_cachable_CRT;
+    
+    `ifdef CACHE_SYSTEM
+        assign cache_ctrl_bufferable_o = data_bufferable_CRT;
+        assign cache_ctrl_cachable_o = data_cachable_CRT;
+    `else 
+        assign data_bufferable_o = data_bufferable_CRT;
+    `endif 
 
 
     /* Sampled when a valid operation is supplied to provide a stable
@@ -117,9 +139,11 @@ module store_unit (
             end
         end
 
-    assign store_data_o = store_data_CRT;
-    assign store_width_o = store_width_CRT;
-    assign store_address_o = store_address_CRT;
+    `ifdef CACHE_SYSTEM
+        assign cache_ctrl_data_o = store_data_CRT;
+        assign cache_ctrl_store_width_o = store_width_CRT;
+        assign cache_ctrl_address_o = store_address_CRT;
+    `endif 
 
 
 //-------------//
@@ -139,26 +163,25 @@ module store_unit (
         end : state_register
 
 
-    assign idle_o = (state_NXT == IDLE);
-
         always_comb begin
             /* Default values */
             state_NXT = state_CRT;
             store_data_NXT = store_data_CRT;
             store_width_NXT = store_width_CRT;
-            data_cachable_NXT = data_cachable_CRT;
             store_address_NXT = store_address_CRT;
-            write_request_NXT = write_request_CRT;
             data_bufferable_NXT = data_bufferable_CRT;
 
             `ifdef CACHE_SYSTEM
-                cache_ctrl_write_o = 1'b0;
+                data_cachable_NXT = data_cachable_CRT;
+                cache_ctrl_store_o = 1'b0;
             `else 
-                memory_ctrl_store_o = 1'b0;
+                str_buf_push_data_o = 1'b0;
+                str_buf_packet_o = '0;
             `endif 
 
             idle_o = 1'b0;
             data_valid_o = 1'b0;
+            illegal_access_o = 1'b0;
 
             case (state_CRT)
 
@@ -167,7 +190,7 @@ module store_unit (
 
                     if (valid_operation_i) begin 
                         /* Exception detection */
-                        if (!writable) begin
+                        if (!accessable) begin
                             state_NXT = EXCEPTION;
                         end else 
                     `ifdef CACHE_SYSTEM
@@ -176,31 +199,33 @@ module store_unit (
                             cache_ctrl_store_o = 1'b1;
                         end
                     `else  
-                        if (memory_ctrl_full_o) begin 
+                        if (str_buf_full_i) begin 
                             state_NXT = WAIT;
-                            memory_ctrl_store_o = 1'b1;
+                            str_buf_push_data_o = 1'b1;
                         end
                     `endif 
 
                         /* Stable signals */
                         store_data_NXT = store_data_i;
-                        instr_packet_NXT = instr_packet_i;
                         store_address_NXT = store_address_i;
                         data_bufferable_NXT = bufferable;
-                        data_cachable_NXT = cachable;
+
+                        `ifdef CACHE_SYSTEM
+                            data_cachable_NXT = cachable;
+                        `endif 
 
                         case (operation_i)
-                            SB: store_width_NXT = BYTE;
+                            STB: store_width_NXT = BYTE;
 
-                            SH: store_width_NXT = HALF_WORD;
+                            STH: store_width_NXT = HALF_WORD;
 
-                            SW: store_width_NXT = WORD;
+                            STW: store_width_NXT = WORD;
                         endcase
                     end
                 end
 
 
-                WAIT_CACHE: begin
+                WAIT: begin
                     `ifdef CACHE_SYSTEM
                         cache_ctrl_store_o = 1'b1; 
 
@@ -208,9 +233,11 @@ module store_unit (
                             state_NXT = STORE_COMMIT;
                         end
                     `else 
-                        memory_ctrl_store_o = 1'b1;
 
-                        if (!memory_ctrl_full_o) begin
+                        if (!str_buf_full_i) begin
+                            str_buf_push_data_o = 1'b1;
+                            str_buf_packet_o = {store_data_CRT, store_address_CRT, store_width_CRT};
+
                             state_NXT = STORE_COMMIT;
                         end
                     `endif 
@@ -236,6 +263,12 @@ module store_unit (
                 end
             endcase
         end 
+
+    `ifdef CACHE_SYSTEM
+        assign cache_ctrl_data_o = store_data_CRT;
+        assign cache_ctrl_address_o = store_address_CRT;
+        assign cache_ctrl_store_width_o = store_width_CRT;
+    `endif 
 
 endmodule : store_unit 
 
