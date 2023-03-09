3bdab345ad1ab0c96c36034bfe07e47b7eda34b4
GabbedT <gabrieletripi02@gmail.com>
2023-03-10 00:16:06 +0100

    Add MMIO timer

--- Hardware/Include/Headers/apogeo_memory_map.svh
+++ Hardware/Include/Headers/apogeo_memory_map.svh
@@ -8,6 +8,8 @@
     /* 
      *  Boot memory region 
      *
+     *  (Only M mode)
+     *
      *  NON CACHABLE
      *  NON WRITABLE
      *  NON BUFFERABLE
@@ -19,11 +21,17 @@
     /* 
      *  Input / Output memory region 
      *
+     *  (Only M mode)
+     *
      *  NON CACHABLE
      *  NON BUFFERABLE
      */
-    `define IO_START `BOOT_END + 1'b1
+    `define IO_START `BOOT_END + 1
     `define IO_END   `IO_START + `KILO(128)
+
+    /* Timer has 8 registers */
+    `define TIMER_START `IO_START
+    `define TIMER_END `IO_START + 7
 
 
     /* 
