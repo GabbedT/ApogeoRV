94ac5e64e179add6f2ab5233e6b37df0957488da
GabbedT <gabrieletripi02@gmail.com>
2023-04-14 13:59:31 +0200

    Updated and completed store buffer

--- Hardware/Memory System/External Memory Interface/store_buffer.sv
+++ Hardware/Memory System/External Memory Interface/store_buffer.sv
@@ -47,38 +47,69 @@
 
 `include "../../Include/Interfaces/store_buffer_interface.sv"
 
-module store_buffer (
+module store_buffer #(
+    /* Number of entries contained */
+    parameter BUFFER_DEPTH = `ST_BUF_DEPTH
+) (
     input logic clk_i,
     input logic rst_n_i,
+    input logic flush_i,
     
     store_buffer_push_interface.slave push_channel,
     store_buffer_pull_interface.slave pull_channel,
 
+    /* Operation tag */
+    input logic [5:0] tag_i,
+
+    /* Validate */
+    input logic valid_i,
+    input logic [5:0] valid_tag_i,
+
     /* Foward data nets */
-    input  data_word_t foward_address_i,
+    input data_word_t foward_address_i,
     output data_word_t foward_data_o,
-    output logic       address_match_o
+    output logic address_match_o,
+
+    /* Head valid */
+    output logic valid_o
 );
 
 //====================================================================================
 //      FIFO LOGIC
 //====================================================================================
 
+    /* Flush logic */
+    logic [$clog2(BUFFER_DEPTH) - 1:0] valid_ptr;
+
+        always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : valid_pointer_register
+            if (!rst_n_i) begin
+                valid_ptr <= '0;
+            end else begin 
+                if (valid_i) begin
+                    valid_ptr <= valid_ptr + 1'b1;
+                end
+            end 
+        end : valid_pointer_register
+
+
     /* Write and read pointers */
-    logic [$clog2(`ST_BUF_DEPTH) - 1:0] push_ptr, inc_push_ptr, pull_ptr, inc_pull_ptr;
+    logic [$clog2(BUFFER_DEPTH) - 1:0] push_ptr, inc_push_ptr, pull_ptr, inc_pull_ptr;
 
     assign inc_push_ptr = push_ptr + 1'b1;
     assign inc_pull_ptr = pull_ptr + 1'b1;
 
-    logic merge_done;
-    
         always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : pointers_register
             if (!rst_n_i) begin
                 pull_ptr <= '0;
                 push_ptr <= '0; 
+            end else if (flush_i) begin 
+                /* Push pointer is setted to the last 
+                 * validated value */ 
+                push_ptr <= valid_ptr;
+                pull_ptr <= pull_ptr;
             end else begin 
                 /* Increment pointer */
-                if (push_channel.push_request & !merge_done) begin
+                if (push_channel.push_request) begin
                     push_ptr <= inc_push_ptr;
                 end
 
@@ -97,6 +128,9 @@ module store_buffer (
             if (!rst_n_i) begin 
                 push_channel.full <= 1'b0;
                 pull_channel.empty <= 1'b1;
+            end else if (flush_i) begin 
+                push_channel.full <= 1'b0;
+                pull_channel.empty <= 1'b1;
             end else begin 
                 case ({push_channel.push_request, pull_channel.pull_request})
                     PULL: begin
@@ -105,58 +139,55 @@ module store_buffer (
                     end
 
                     PUSH: begin
-                        /* Merging doesn't increase FIFO data */
-                        if (!merge_done) begin
-                            pull_channel.empty <= 1'b0;
-                            push_channel.full <= (pull_ptr == inc_push_ptr);
-                        end
+                        pull_channel.empty <= 1'b0;
+                        push_channel.full <= (pull_ptr == inc_push_ptr);
                     end
                 endcase 
             end
         end : status_register
 
-    logic [$clog2(`ST_BUF_DEPTH) - 1:0] foward_ptr, merge_ptr;
+    logic [$clog2(BUFFER_DEPTH) - 1:0] foward_ptr;
+
+    /* Valid pointer indicates that all the previous values in
+     * the buffer are valid. */
+    assign valid_o = (pull_ptr < valid_ptr);
 
 
 //====================================================================================
 //      DATA BUFFER MEMORY
 //====================================================================================
-    
-    typedef struct packed {
-        data_word_t   data;
-        store_width_t store_width;
-    } data_t;
 
     /* Implemented with a memory with 1W and 2R ports 
      * to avoid conflicts between fowarding and pulling */
-    data_t data_buffer [`ST_BUF_DEPTH - 1:0];
+    logic [$bits(data_word_t) - 1:0] data_buffer [1:0][BUFFER_DEPTH - 1:0];
 
         always_ff @(posedge clk_i) begin : write_data_port
             if (push_channel.push_request) begin
-                if (merge_done) begin
-                    /* Merge has priority over pushing */
-                    case (push_channel.packet.store_width)
-                        WORD: data_buffer[merge_ptr].data <= push_channel.packet.data;
-
-                        HALF_WORD: data_buffer[merge_ptr].data.word16[push_channel.packet.address[1]] <= push_channel.packet.data[15:0];
-
-                        BYTE: data_buffer[merge_ptr].data.word16[push_channel.packet.address[1:0]] <= push_channel.packet.data[7:0];
-                    endcase 
-
-                    data_buffer[merge_ptr].store_width <= push_channel.packet.store_width;
-                end else begin
-                    /* Push data */
-                    data_buffer[push_ptr] <= {push_channel.packet.data, push_channel.packet.store_width};
-                end
+                /* Push data */
+                data_buffer[0][push_ptr] <= push_channel.packet.data;
+                data_buffer[1][push_ptr] <= push_channel.packet.data;
             end
         end : write_data_port
 
     /* Foward read port */
-    assign foward_data_o = data_buffer[foward_ptr].data;
+    assign foward_data_o = data_buffer[1][foward_ptr];
 
     /* Pull read port */
-    assign pull_channel.packet.data = data_buffer[pull_ptr].data;
-    assign pull_channel.packet.store_width = data_buffer[pull_ptr].store_width;
+    assign pull_channel.packet.data = data_buffer[0][pull_ptr];
+
+
+    /* Implemented with a memory with 1W and 1R port */
+    logic [$bits(store_width_t) - 1:0] store_width_buffer [BUFFER_DEPTH - 1:0];
+
+        always_ff @(posedge clk_i) begin : write_store_width_port
+            if (push_channel.push_request) begin
+                /* Push data */
+                store_width_buffer[push_ptr] <= push_channel.packet.store_width;
+            end
+        end : write_store_width_port
+
+    /* Read port */
+    assign pull_channel.packet.store_width = store_width_buffer[pull_ptr];
 
 
 //====================================================================================
@@ -168,62 +199,61 @@ module store_buffer (
         data_word_t address;
     } metadata_t;
 
+
     /* Implemented with flip-flops since every entry needs to
      * be accessed simultaneously */
-    metadata_t  metadata_buffer [`ST_BUF_DEPTH - 1:0];
+    metadata_t metadata_buffer [BUFFER_DEPTH - 1:0];
 
     /* Initialize data */
     initial begin
-        for (int i = 0; i < `ST_BUF_DEPTH; ++i) begin 
-            metadata_buffer[i] = '0;
+        for (int i = 0; i < BUFFER_DEPTH; ++i) begin 
+            metadata_buffer[i].valid = '0;
         end
     end
 
-        always_ff @(posedge clk_i) begin : write_metadata_port
+        always_ff @(posedge clk_i) begin : write_foward_metadata_port
             if (push_channel.push_request) begin
-                /* Validate on push */
-                metadata_buffer[push_ptr].valid <= 1'b1;
-
-                if (merge_done) begin
-                    /* Merge has priority over pushing */
-                    metadata_buffer[merge_ptr].address <= push_channel.packet.address;
-                end else begin
-                    /* Push data */
-                    metadata_buffer[push_ptr].address <= push_channel.packet.address;
-                end
+                /* Push data */
+                metadata_buffer[push_ptr].address <= push_channel.packet.address;
             end
-            
-            if (pull_channel.pull_request) begin
-                /* Invalidate on pull */
-                metadata_buffer[pull_ptr].valid <= 1'b0;
-            end
-        end : write_metadata_port
+        end : write_foward_metadata_port
 
     assign pull_channel.packet.address = metadata_buffer[pull_ptr].address;
+
+
+        always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : write_foward_metadata_valid_port
+            if (!rst_n_i) begin 
+                for (int i = 0; i < BUFFER_DEPTH; ++i) begin 
+                    metadata_buffer[i].valid <= '0;
+                end
+            end else begin 
+                if (pull_channel.pull_request) begin
+                    /* Invalidate on pull */
+                    metadata_buffer[push_ptr].valid <= 1'b0;
+                end
+                
+                if (push_channel.push_request) begin
+                    /* Validate on push */
+                    metadata_buffer[push_ptr].valid <= 1'b1;
+                end
+            end 
+        end : write_foward_metadata_valid_port
 
 
 //====================================================================================
 //      MERGE AND FOWARD LOGIC
 //====================================================================================
 
-    logic [`ST_BUF_DEPTH - 1:0] push_address_match, pop_address_match;
+    logic [BUFFER_DEPTH - 1:0] pop_address_match;
 
         always_comb begin : address_match_logic
             /* Default values */
             foward_ptr = '0;
-            merge_ptr = '0;
+            pop_address_match = '0;
 
-            for (int i = 0; i < `ST_BUF_DEPTH; ++i) begin
-                /* Check if any write address match an entry */
-                push_address_match[i] = (push_channel.packet.address[31:2] == metadata_buffer[i].address[31:2]) & metadata_buffer[i].valid;
-
+            for (int i = BUFFER_DEPTH - 1; i >= 0; --i) begin
                 /* Check if any read address match an entry */
-                pop_address_match[i] = (foward_address_i[31:2] == metadata_buffer[i].address[31:2]) & metadata_buffer[i].valid;
-
-                /* Generate pointer based on address match */ 
-                if (push_address_match[i]) begin
-                    merge_ptr = i;
-                end
+                pop_address_match[i] = (foward_address_i == metadata_buffer[i].address) & metadata_buffer[i].valid;
                 
                 if (pop_address_match[i]) begin
                     foward_ptr = i;
@@ -231,7 +261,6 @@ module store_buffer (
             end
         end : address_match_logic
 
-    assign merge_done = (push_address_match != '0) & push_channel.push_request;
     assign address_match_o = (pop_address_match != '0);
 
 endmodule : store_buffer
