a548602c6a3cc11a7e0bf6c3ba84b5b729e838f4
GabbedT <gabrieletripi02@gmail.com>
2023-04-18 01:15:10 +0200

    Rename

---
+++ Hardware/Front End/decoder.sv
@@ -0,0 +1,210 @@
+`ifndef DECODER_SV
+    `define DECODER_SV
+
+`include "../Include/Headers/apogeo_exception_vectors.svh"
+`include "../Include/Headers/apogeo_configuration.svh"
+
+`include "../Include/Packages/riscv_instructions_pkg.sv"
+`include "../Include/Packages/apogeo_pkg.sv"
+`include "../Include/Packages/apogeo_operations_pkg.sv"
+
+`include "Decoder/integer_decoder.sv"
+
+`ifdef BMU 
+    `include "Decoder/bit_manipulation_decoder.sv"
+`endif 
+
+module decoder (
+    /* Instruction supplied by the fetch 
+     * buffer */
+    input riscv32::instruction_t instr_i,
+    input data_word_t instr_address_i,
+
+    /* Privilege level for system call */
+    input logic priv_level_i,
+
+    /* Immediate */
+    output data_word_t [1:0] immediate_o,
+    output logic [1:0] imm_valid_o,
+
+    /* Instruction is a jump and require 
+     * to set the PC to the BTA */
+    output logic branch_o,
+
+    /* Require the current PC + 4 to be 
+     * passed as operand */
+    output logic jump_o,
+
+    /* Calculate memory address (base + offset) 
+     * and use as first operand for the units */
+    output logic memory_o,
+    output logic [1:0] address_operand_o,
+
+    /* Stall the front end until the execution
+     * pipeline is empty */
+    output logic fence_o,
+
+    /* Registers */
+    output logic [1:0][4:0] reg_src_o,
+    output logic [4:0] reg_dest_o,
+
+    /* Micro instructions */
+    output exu_valid_t exu_valid_o,
+    output exu_uop_t exu_uop_o,
+
+    /* Exception */
+    output logic exception_generated_o,
+    output logic [4:0] exception_vector_o
+);
+
+
+//====================================================================================
+//      INTEGER DECODER
+//====================================================================================
+
+    /* Valid unit */
+    itu_valid_t itu_valid_idec; 
+    lsu_valid_t lsu_valid_idec; 
+    logic csr_valid_idec; 
+
+    /* Unit micro instruction */
+    itu_uop_t itu_uop_idec;
+    lsu_uop_t lsu_uop_idec;
+    csr_uop_t csr_uop_idec;
+
+    /* Immediates */
+    data_word_t [1:0] immediate_idec;
+    logic [1:0] is_immediate_idec;
+
+    /* Flags */
+    logic is_memory_idec, exc_gen_idec; 
+
+    /* Registers */
+    logic [1:0][4:0] reg_src_idec; logic [4:0] reg_dest_idec;
+    
+    /* Exception vector */
+    logic [4:0] exc_vect_idec;
+
+    integer_decoder idecoder (
+        .instr_i         ( instr_i         ),
+        .instr_address_i ( instr_address_i ),
+
+        .priv_level_i     ( priv_level_i     ),
+
+        .itu_unit_valid_o ( itu_valid_idec ),
+        .itu_unit_uop_o   ( itu_uop_idec   ),
+        .lsu_unit_valid_o ( lsu_valid_idec ),
+        .lsu_unit_uop_o   ( lsu_uop_idec   ),
+        .csr_unit_valid_o ( csr_valid_idec ),
+        .csr_unit_uop_o   ( csr_uop_idec   ),
+
+        .immediate_o       ( immediate_idec       ),
+        .imm_valid_o       ( is_immediate_idec    ),
+        .address_operand_o ( address_operand_o ),
+
+        .branch_o ( branch_o ),
+        .jump_o   ( jump_o   ),
+        .memory_o ( memory_o ),
+        .fence_o  ( fence_o  ),
+
+        .reg_src_o  ( reg_src_idec  ),
+        .reg_dest_o ( reg_dest_idec ),
+
+        .exception_generated_o ( exc_gen_idec  ),
+        .exception_vector_o    ( exc_vect_idec )
+    ); 
+
+
+//====================================================================================
+//      BIT MANIPULATION DECODER
+//====================================================================================
+
+    `ifdef BMU 
+
+    /* Valid unit */
+    logic bmu_valid_bdec; 
+
+    /* Unit micro instruction */
+    bmu_uop_t bmu_uop_bdec;
+
+    /* Immediates */
+    data_word_t immediate_bdec;
+    logic is_immediate_bdec;
+
+    /* Flag */
+    logic exc_gen_bdec; 
+
+    /* Registers */
+    logic [1:0][4:0] reg_src_bdec; logic [4:0] reg_dest_bdec;
+    
+    /* Exception vector */
+    logic [4:0] exc_vect_bdec;
+
+    bit_manipulation_decoder bdecoder (
+        .instr_i ( instr_i ),
+
+        .immediate_o ( immediate_bdec    ),
+        .imm_valid_o ( is_immediate_bdec ),
+
+        .reg_src_o  ( reg_src_bdec  ),
+        .reg_dest_o ( reg_dest_bdec ),
+
+        .unit_valid_o ( bmu_valid_bdec ),
+        .unit_uop_o   ( bmu_uop_bdec   ),
+
+        .exception_generated_o ( exc_gen_bdec  ),
+        .exception_vector_o    ( exc_vect_bdec )
+    ); 
+
+    `endif 
+
+
+//====================================================================================
+//      OUTPUT LOGIC
+//====================================================================================
+
+    assign immediate_o[0] = immediate_idec[0] `ifdef BMU | immediate_bdec `endif;
+    assign immediate_o[1] = immediate_idec[1];
+
+    assign imm_valid_o[0] = is_immediate_idec[0] `ifdef BMU | immediate_bdec `endif;
+    assign imm_valid_o[1] = is_immediate_idec[1];
+
+    assign reg_src_o[0] = reg_src_idec[0] `ifdef BMU | reg_src_bdec[0] `endif;
+    assign reg_src_o[1] = reg_src_idec[1] `ifdef BMU | reg_src_bdec[1] `endif;
+
+    assign reg_dest_o = reg_dest_idec `ifdef BMU | reg_dest_bdec `endif;
+
+    assign exu_valid_o.ITU = itu_valid_idec `ifdef BMU | {1'b0, bmu_valid_bdec, 2'b0} `endif; 
+    assign exu_valid_o.LSU = lsu_valid_idec;
+    assign exu_valid_o.CSR = csr_valid_idec;
+
+    exu_uop_t itu_uop, lsu_uop, csr_uop;
+
+    assign itu_uop.ITU.subunit = itu_uop_idec `ifdef BMU | bmu_uop_bdec `endif; 
+    assign itu_uop.ITU.padding = '0; 
+    assign lsu_uop.LSU.subunit = lsu_uop_idec;
+    assign lsu_uop.LSU.padding = '0;
+    assign csr_uop.CSR.subunit = csr_uop_idec;
+    assign csr_uop.CSR.padding = '0;
+
+    assign exu_uop_o = itu_uop | lsu_uop | csr_uop;
+
+    assign exception_generated_o = exc_gen_idec `ifdef BMU & exc_gen_bdec `endif;
+    
+    `ifdef BMU 
+        always_comb begin 
+            if (exc_gen_idec) begin    
+                exception_vector_o = exc_vect_idec;
+            end else if (exc_gen_bdec) begin
+                exception_vector_o = exc_vect_bdec;
+            end else begin
+                exception_vector_o = '0;
+            end
+        end
+    `else 
+        assign exception_vector_o = exc_vect_idec;
+    `endif 
+
+endmodule : decoder
+
+`endif 
---
+++ Hardware/Front End/scoreboard.sv
@@ -0,0 +1,427 @@
+// MIT License
+//
+// Copyright (c) 2021 Gabriele Tripi
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// -------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
+// FILE NAME : scoreboard.sv
+// DEPARTMENT : 
+// AUTHOR : Gabriele Tripi
+// AUTHOR'S EMAIL : tripi.gabriele2002@gmail.com
+// -------------------------------------------------------------------------------------
+// RELEASE HISTORY
+// VERSION : 1.0 
+// DESCRIPTION : This scoreboard is the unit that grant ordering and coherency in the
+//               execution pipeline. It serves three main purpouses: 
+//
+//               - Only one unit must produce a valid result in the same clock cycle.
+//               - Checks for RAW hazards .
+//               - Checks for structural hazard for sequential units.
+//
+//               If an hazard occours, the scoreboard block all the front end.
+//               For each unit, it keeps track of the number of cycles remaining before
+//               a valid result is produced. For pipelined units a counter is associated
+//               to each pipeline stage. 
+//               Also the destination register is memorized for RAW hazard check and
+//               to ensure that no more than 1 destination register is in flight in the
+//               execution stage.  
+// -------------------------------------------------------------------------------------
+
+`ifndef SCOREBOARD_SV
+    `define SCOREBOARD_SV
+
+`include "../Include/Headers/apogeo_configuration.svh"
+`include "../Include/Packages/apogeo_operations_pkg.sv"
+
+module scoreboard (
+    input logic clk_i,
+    input logic rst_n_i,
+    input logic flush_i,
+    input logic stall_i,
+
+    /* Registers addresses */
+    input logic [1:0][4:0] src_reg_i,
+    input logic [4:0] dest_reg_i,
+
+    /* Functional units */
+    input logic csr_unit_i,
+    input itu_valid_t itu_unit_i,
+    input lsu_valid_t lsu_unit_i,
+
+    /* Issue command */
+    output logic pipeline_empty_o,
+    output logic issue_instruction_o
+);
+
+//====================================================================================
+//      PARAMETERS AND FUNCTIONS
+//====================================================================================  
+
+    /* Since before the execution stage there's a bypass stage
+     * the latencies must be increased by 1 */
+
+    /* Valid for ALU and CSR */
+    localparam COMBINATIONAL = 1;
+
+    localparam MUL_LATENCY = 6;
+
+    localparam BMU_LATENCY = 2;
+
+    localparam DIV_LATENCY = 36;
+
+    /* Check if the dispatched instruction is being issued in the next cycle */
+    function bit issue_next_cycle(input bit unit_to_issue);
+
+        return unit_to_issue & issue_instruction_o;
+
+    endfunction : issue_next_cycle
+
+
+//====================================================================================
+//      LATENCY CHECK LOGIC
+//==================================================================================== 
+
+    logic [5:0] latency; 
+
+        always_comb begin : latency_assignment
+            /* Integer unit */
+            if (itu_unit_i.MUL) begin
+                latency = MUL_LATENCY + 1; 
+            end else if (itu_unit_i.DIV) begin
+                latency = DIV_LATENCY + 1; 
+            end else if (itu_unit_i.ALU | csr_unit_i) begin
+                latency = COMBINATIONAL + 1;
+            end 
+
+            `ifdef BMU else if (itu_unit_i.BMU) begin
+                latency = BMU_LATENCY + 1; 
+            end 
+            `endif
+
+            else begin
+                latency = '1;
+            end
+        end : latency_assignment
+
+
+//====================================================================================
+//      MUL SCHEDULING LOGIC
+//====================================================================================  
+
+    /* Select the multiplication stage */
+    logic [MUL_LATENCY - 1:0] mul_stage; 
+
+        always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : mul_stage_selector
+            if (!rst_n_i) begin
+                mul_stage <= 1'b1;
+            end else if (flush_i) begin
+                mul_stage <= 1'b1;
+            end else if (!stall_i & issue_next_cycle(itu_unit_i.MUL)) begin
+                if (mul_stage[MUL_LATENCY - 1]) begin
+                    /* Wrap around the shifted bit */
+                    mul_stage <= 1'b1;
+                end else begin 
+                    /* Shift the bit every time an
+                     * operation arrives */
+                    mul_stage <= mul_stage << 1;
+                end 
+            end 
+        end : mul_stage_selector
+
+
+    /* Since MUL is a pipelined unit, the scoreboard needs to keep 
+     * track of every stage */
+    logic [MUL_LATENCY - 1:0] mul_executing, mul_raw_hazard, mul_latency_hazard;
+    logic [MUL_LATENCY - 1:0][31:0] mul_register_dest;
+    logic [MUL_LATENCY - 1:0][$clog2(MUL_LATENCY) - 1:0] mul_latency_cnt;
+
+    generate genvar i;
+
+        for (i = 0; i < MUL_LATENCY; ++i) begin 
+            always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : mul_status_register
+                if (!rst_n_i) begin
+                    mul_latency_cnt[i] <= '1;
+                end else if (flush_i) begin
+                    mul_latency_cnt[i] <= '1;
+                end else if (!stall_i) begin 
+                    if (issue_next_cycle(itu_unit_i.MUL) & mul_stage[i]) begin
+                        /* If the current stage counter is selected 
+                         * load status */
+                        mul_latency_cnt[i] <= MUL_LATENCY;
+                    end else if (mul_latency_cnt[i] != '0) begin
+                        /* Keep decrementing the latency counter until the
+                         * unit produces a valid result */
+                        mul_latency_cnt[i] <= mul_latency_cnt[i] - 1'b1;
+                    end else begin
+                        /* The unit has finished */
+                        mul_latency_cnt[i] <= '0;
+                    end
+                end
+            end : mul_status_register
+
+            always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : mul_destination_register
+                if (!rst_n_i) begin
+                    mul_register_dest[i] <= '0;
+                end else if (!stall_i) begin 
+                    if (issue_next_cycle(itu_unit_i.MUL) & mul_stage[i]) begin 
+                        /* Load register in the next cycle if the instruction 
+                         * dispatched is being issued in the next cycle */
+                        mul_register_dest[i] <= dest_reg_i;
+                    end 
+                end
+            end : mul_destination_register
+
+            assign mul_executing[i] = (mul_latency_cnt[i] != '0);
+
+            assign mul_raw_hazard[i] = ((src_reg_i[0] == mul_register_dest[i]) | (src_reg_i[1] == mul_register_dest[i]) | (dest_reg_i == mul_register_dest[i])) & 
+                                        mul_executing[i] & (mul_register_dest[i] != '0);
+
+            assign mul_latency_hazard[i] = (latency == mul_latency_cnt[i]) & mul_executing[i];
+
+        end 
+
+    endgenerate
+
+
+//====================================================================================
+//      BMU SCHEDULING LOGIC
+//====================================================================================  
+    
+    `ifdef BMU 
+
+    /* Select the bit manipulation stage */
+    logic [BMU_LATENCY - 1:0] bmu_stage; 
+
+        always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : bmu_stage_selector
+            if (!rst_n_i) begin
+                bmu_stage <= 1'b1;
+            end else if (flush_i) begin
+                bmu_stage <= 1'b1;
+            end else if (!stall_i & issue_next_cycle(itu_unit_i.BMU)) begin
+                if (bmu_stage[BMU_LATENCY - 1]) begin
+                    /* Wrap around the shifted bit */
+                    bmu_stage <= 1'b1;
+                end else begin 
+                    /* Shift the bit every time an
+                     * operation arrives */
+                    bmu_stage <= bmu_stage << 1;
+                end 
+            end 
+        end : bmu_stage_selector
+
+
+    /* Since BMU is a pipelined unit, the scoreboard needs to keep 
+     * track of every stage */
+    logic [BMU_LATENCY - 1:0] bmu_executing, bmu_raw_hazard, bmu_latency_hazard;
+    logic [BMU_LATENCY - 1:0][31:0] bmu_register_dest;
+    logic [BMU_LATENCY - 1:0][$clog2(BMU_LATENCY):0] bmu_latency_cnt;
+
+    generate;
+
+        for (i = 0; i < BMU_LATENCY; ++i) begin 
+            always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : bmu_status_register
+                if (!rst_n_i) begin
+                    bmu_latency_cnt[i] <= '1;
+                end else if (flush_i) begin
+                    bmu_latency_cnt[i] <= '1;
+                end else if (!stall_i) begin 
+                    if (issue_next_cycle(itu_unit_i.BMU) & bmu_stage[i]) begin
+                        /* If the current stage counter is selected 
+                         * load status */
+                        bmu_latency_cnt[i] <= BMU_LATENCY;
+                    end else if (bmu_latency_cnt[i] != '0) begin
+                        /* Keep decrementing the latency counter until the
+                         * unit produces a valid result */
+                        bmu_latency_cnt[i] <= bmu_latency_cnt[i] - 1'b1;
+                    end else begin
+                        /* The unit has finished */
+                        bmu_latency_cnt[i] <= '0;
+                    end
+                end
+            end : bmu_status_register
+
+            always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : bmu_destination_register
+                if (!rst_n_i) begin
+                    bmu_register_dest[i] <= '0;
+                end else if (!stall_i) begin 
+                    if (issue_next_cycle(itu_unit_i.BMU) & bmu_stage[i]) begin 
+                        bmu_register_dest[i] <= dest_reg_i;
+                    end 
+                end
+            end : bmu_destination_register
+
+            assign bmu_executing[i] = (bmu_latency_cnt[i] != '0);
+            
+            assign bmu_raw_hazard[i] = ((src_reg_i[0] == bmu_register_dest[i]) | (src_reg_i[1] == bmu_register_dest[i]) | (dest_reg_i == bmu_register_dest[i])) & 
+                                        bmu_executing[i] & (bmu_register_dest[i] != '0);
+
+            assign bmu_latency_hazard[i] = (latency == bmu_latency_cnt[i]) & bmu_executing[i];
+
+        end
+
+    endgenerate
+    
+    `endif 
+
+//====================================================================================
+//      DIV SCHEDULING LOGIC
+//==================================================================================== 
+
+    logic div_executing, div_raw_hazard, div_latency_hazard;
+    logic [31:0] div_register_dest;
+    logic [$clog2(DIV_LATENCY) - 1:0] div_latency_cnt;
+
+        always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : div_status_register
+            if (!rst_n_i) begin
+                div_latency_cnt <= '0;
+            end else if (flush_i) begin
+                div_latency_cnt <= '0;
+            end else if (!stall_i) begin
+                if (issue_next_cycle(itu_unit_i.DIV)) begin
+                    div_latency_cnt <= DIV_LATENCY;
+                end else if (div_latency_cnt != '0) begin
+                    /* Keep decrementing the latency counter until the
+                     * unit produces a valid result */
+                    div_latency_cnt <= div_latency_cnt - 1'b1;
+                end else begin
+                    /* The unit has finished */
+                    div_latency_cnt <= '0;
+                end
+            end
+        end : div_status_register
+
+        always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : div_destination_register
+            if (!rst_n_i) begin
+                div_register_dest <= '0;
+            end else if (!stall_i) begin 
+                if (issue_next_cycle(itu_unit_i.DIV)) begin 
+                    div_register_dest <= dest_reg_i;
+                end 
+            end
+        end : div_destination_register
+
+    assign div_executing = (div_latency_cnt != '0);
+
+    assign div_raw_hazard = ((src_reg_i[0] == div_register_dest) | (src_reg_i[1] == div_register_dest) | (dest_reg_i == div_register_dest)) & div_executing & (div_register_dest != '0);
+
+    assign div_latency_hazard = (latency == div_latency_cnt) & div_executing;
+
+
+//====================================================================================
+//      LDU SCHEDULING LOGIC
+//==================================================================================== 
+
+    logic ldu_raw_hazard, ldu_executing;
+    logic [31:0] ldu_register_dest;
+
+        always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : ldu_status_register
+            if (!rst_n_i) begin
+                ldu_executing <= 1'b0;
+            end else if (flush_i) begin
+                ldu_executing <= 1'b0;
+            end else if (!stall_i) begin
+                if (issue_next_cycle(lsu_unit_i.LDU)) begin
+                    ldu_executing <= 1'b1;
+                end else if (div_latency_cnt != '0) begin
+                    /* Keep decrementing the latency counter until the
+                     * unit produces a valid result */
+                    ldu_executing <= 1'b1;
+                end else begin
+                    /* The unit has finished */
+                    ldu_executing <= 1'b0;
+                end
+            end
+        end : ldu_status_register
+
+        always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : ldu_destination_register
+            if (!rst_n_i) begin
+                ldu_register_dest <= '0;
+            end else if (!stall_i) begin
+                if (issue_next_cycle(lsu_unit_i.LDU)) begin 
+                    ldu_register_dest <= dest_reg_i;
+                end 
+            end
+        end : ldu_destination_register
+
+    assign ldu_raw_hazard = ((src_reg_i[0] == ldu_register_dest) | (dest_reg_i == ldu_register_dest)) & ldu_executing & (ldu_register_dest != '0);
+
+
+//====================================================================================
+//      STU SCHEDULING LOGIC
+//==================================================================================== 
+
+    logic stu_raw_hazard, stu_executing;
+    logic [31:0] stu_register_dest;
+
+        always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : stu_status_register
+            if (!rst_n_i) begin
+                stu_executing <= 1'b0;
+            end else if (flush_i) begin
+                stu_executing <= 1'b0;
+            end else if (!stall_i) begin
+                if (issue_next_cycle(lsu_unit_i.STU)) begin
+                    stu_executing <= 1'b1;
+                end else if (div_latency_cnt != '0) begin
+                    /* Keep decrementing the latency counter until the
+                     * unit produces a valid result */
+                    stu_executing <= 1'b1;
+                end else begin
+                    /* The unit has finished */
+                    stu_executing <= 1'b0;
+                end
+            end
+        end : stu_status_register
+
+        always_ff @(posedge clk_i `ifdef ASYNC or negedge rst_n_i `endif) begin : stu_destination_register
+            if (!rst_n_i) begin
+                stu_register_dest <= '0;
+            end else if (!stall_i) begin
+                if (issue_next_cycle(lsu_unit_i.STU)) begin 
+                    stu_register_dest <= dest_reg_i;
+                end 
+            end
+        end : stu_destination_register
+
+    assign stu_raw_hazard = ((src_reg_i[0] == stu_register_dest) | (dest_reg_i == stu_register_dest)) & stu_executing & (stu_register_dest != '0); 
+
+
+//====================================================================================
+//      ISSUE LOGIC
+//==================================================================================== 
+
+    logic raw_hazard, latency_hazard, structural_hazard;
+
+    assign raw_hazard = stu_raw_hazard | ldu_raw_hazard | div_raw_hazard | (|mul_raw_hazard) `ifdef BMU | (|bmu_raw_hazard) `endif;
+    assign latency_hazard = div_latency_hazard | (|mul_latency_hazard) `ifdef BMU | (|bmu_latency_hazard) `endif;
+    assign structural_hazard = (itu_unit_i.DIV & div_executing) | (lsu_unit_i.LDU & ldu_executing) | (lsu_unit_i.STU & stu_executing);
+
+
+//====================================================================================
+//      OUTPUT LOGIC
+//==================================================================================== 
+
+    assign issue_instruction_o = !(raw_hazard | latency_hazard | structural_hazard);
+
+    /* If no unit is executing, then the pipeline is empty */
+    assign pipeline_empty_o = !((|mul_executing) | div_executing | `ifdef BMU (|bmu_executing) `endif | stu_executing | ldu_executing);
+
+endmodule : scoreboard
+
+`endif 
