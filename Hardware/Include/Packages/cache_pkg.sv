7422eff61580c0eaa7200bf6991e1a4b201cb6e9
GabbedT <gabrieletripi02@gmail.com>
2023-04-29 11:43:58 +0200

    Added cache package

---
+++ Hardware/Include/Packages/cache_pkg.sv
@@ -0,0 +1,30 @@
+`ifndef CACHE_PKG_SV
+    `define CACHE_PKG_SV 
+
+package cache_pkg; 
+
+    /* Transaction packet */
+    typedef struct packed {
+        /* Valid bit is used to signal if the current block 
+         * holds valid data */
+        logic valid;
+
+        /* Dirty bit is used to signal that the current block 
+         * has been written and memory is not up to date */
+        logic dirty;
+    } status_packet_t;
+
+
+    /* Access memory chip */
+    typedef struct packed {
+        logic valid;
+        logic dirty;
+        logic tag;
+        logic data;
+    } enable_t;
+
+endpackage : cache_pkg 
+
+import cache_pkg::*;
+
+`endif 
