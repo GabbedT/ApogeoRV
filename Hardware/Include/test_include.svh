429a4189cebc6326b7786d732c288934db9e6e86
GabbedT <gabrieletripi02@gmail.com>
2023-07-31 12:03:09 +0200

    Increased debug granularity

--- Hardware/Include/test_include.svh
+++ Hardware/Include/test_include.svh
@@ -1,6 +1,14 @@
 `ifndef TEST_INCLUDE_SV
     `define TEST_INCLUDE_SV
 
-// `define TEST_DESIGN
+// `define DECOMPRESSOR_DEBUG
+
+// `define IDECODER_DEBUG
+
+// `define BDECODER_DEBUG
+
+// `define WRITEBACK_DEBUG 
+
+// `define CSR_DEBUG 
 
 `endif
