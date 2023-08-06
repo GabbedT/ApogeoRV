4833f75a78b8d74c2a91cbc8af172175b7280a7d
GabbedT <gabrieletripi02@gmail.com>
2023-08-06 20:44:50 +0200

    Added tests

---
+++ Software/Test/RV32I/Utils/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+cd ../
+
+# Setup the files
+for file in *.S; do 
+    sed -i 's/RVTEST_RV64U/RVTEST_RV32U/' $file 
+    sed -i 's/#include "riscv_test\.h"/#include "Utils\/riscv_test.h"/' $file
+    sed -i 's/#include "test_macros\.h"/#include "Utils\/test_macros.h"/' $file    
+done
+
+for file in *.S; do 
+    filename=${file%.S}
+    
+    riscv32-unknown-elf-gcc -E -o $filename.i $filename.S
+    riscv32-unknown-elf-as -c -mabi=ilp32 -march=rv32i $filename.i -o $filename.o
+    riscv32-unknown-elf-ld -T Utils/link.ld $filename.o -o $filename.elf
+    riscv32-unknown-elf-objcopy -O verilog --verilog-data-width=4 $filename.elf $filename.hex
+    riscv32-unknown-elf-objdump -d $filename.elf > $filename.dump
+
+    rm $filename.i 
+    rm $filename.o 
+    rm $filename.elf
+done 
