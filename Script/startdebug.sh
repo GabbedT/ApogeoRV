4674ca2394a2455e9b0622954f19a3d918a49bf7
GabbedT <gabrieletripi02@gmail.com>
2023-10-16 00:29:51 +0200

    Deleted useless files

--- Script/startdebug.sh
+++
@@ -1,7 +0,0 @@
-riscv32-unknown-elf-gcc -ggdb -nostdlib -nostartfiles -o __prova__ startup.s prova.c 
-
-qemu-riscv32 -g 1234 __prova__
-
-riscv32-unknown-elf-gdb program
-
-(gdb) target remote localhost:1234
