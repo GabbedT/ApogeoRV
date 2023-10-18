5ca8d2c54f4cb295d501293a5e6d7dc7ae96b619
GabbedT <gabrieletripi02@gmail.com>
2023-10-18 17:34:42 +0200

    Readthedocs fix

--- Docs/Sphinx/conf.py
+++ Docs/Sphinx/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Apogeo Documentation'
+project = 'ApogeoCore Documentation'
 copyright = '2023, Tripi Gabriele'
 author = 'Tripi Gabriele'
 release = '20/05/2023'
@@ -19,7 +19,14 @@ extensions = ['sphinx.ext.todo', 'sphinx.ext.autosectionlabel', 'sphinx.ext.auto
 templates_path = ['_templates']
 exclude_patterns = []
 
+# The master toctree document.
+master_doc = 'index'
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
