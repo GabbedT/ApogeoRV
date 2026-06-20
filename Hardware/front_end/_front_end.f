b1a25e67a498c68f22ef388ea2d0016276521ed7
Gabriele Tripi <gabrieletripi02@gmail.com>
2026-06-20 15:41:31 +0200

    Added .f files for front end

---
+++ Hardware/front_end/_front_end.f
@@ -0,0 +1,14 @@
+-F decoder/_decoder.f
+
+branch_predictor/branch_target_buffer.sv
+branch_predictor/predictor_unit.sv
+
+branch_predictor.sv
+decoder.sv
+halt_unit.sv
+instruction_buffer.sv
+register_file.sv
+scoreboard.sv
+scheduler.sv
+
+front_end.sv
---
+++ Hardware/front_end/decoder/_decoder.f
@@ -0,0 +1,5 @@
+decompressor.sv 
+
+bit_manipulation_decoder.sv
+float_decoder.sv
+integer_decoder.sv
