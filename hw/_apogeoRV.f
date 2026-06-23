d3ad217d45b20a4e9189c9d22b7ee10b9e7aa1fd
Gabriele Tripi <gabrieletripi02@gmail.com>
2026-06-23 09:13:54 +0200

    Fixed .f files

--- hw/_apogeoRV.f
+++ hw/_apogeoRV.f
@@ -4,16 +4,16 @@ inc/headers/apogeo_configuration.svh
 inc/headers/apogeo_exception_vectors.svh
 inc/headers/apogeo_memory_map.svh
 
-inc/intf/bus_interface.sv
-inc/intf/store_buffer_interface.sv
-inc/intf/trace_interface.sv
+inc/pkg/riscv_instructions_pkg.sv
+inc/pkg/apogeo_operations_pkg.sv
+inc/pkg/apogeo_pkg.sv
 
 inc/pkg/exu/control_status_registers_pkg.sv
 inc/pkg/exu/store_unit_pkg.sv
 
-inc/pkg/riscv_instructions_pkg.sv
-inc/pkg/apogeo_operations_pkg.sv
-inc/pkg/apogeo_pkg.sv
+inc/intf/bus_interface.sv
+inc/intf/store_buffer_interface.sv
+inc/intf/trace_interface.sv
 
 -F back_end/_back_end.f
 -F front_end/_front_end.f
--- hw/back_end/exu/math/_math.f
+++ hw/back_end/exu/math/_math.f
@@ -1,4 +1,4 @@
-clz/boudary_nibble_encoder.sv
+clz/boundary_nibble_encoder.sv
 clz/nibble_local_count.sv
 clz/count_leading_zeros.sv
 
